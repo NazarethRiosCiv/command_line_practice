d8efd2bc3e95d375a9c153b2cafbe9ccbaa0bfcd
Rios <1556544189157002@mil>
2023-04-10 08:32:19 -0400

    Initial commit - added files

---
+++ introduction.sh
@@ -0,0 +1,28 @@
+#### Intoduction to the Command Line ####
+
+### What is the Command Line ###
+
+# The command line is a text interface for the computer's 
+# operating system. You can use it to traverse and edit your
+# computer's filesystem. 
+
+### Core Functionalities ###
+
+# Navigation: you'll learn how to navigate through a computer's 
+# filesystem solely through the command line. You'll learn how
+# to view the contents of your filesystem, move through the 
+# different directories, and make new files and directories.
+
+# Manipulation: you'll learn how to manipulate the contents of
+# the filesystem with the commmand line. You'll learn how to 
+# move files, delete files and directories, and more.
+
+# Redirection: you'll learn how to redirect standard input, 
+# standard output, and standard error messages. You'll learn
+# how to display the contents of a file, write to that file,
+# search through directories, and more. 
+
+# Configuration: you'll learn how to configure your local 
+# environment. You'll learn how to set and edit your settings
+# and preferences, assign aliases (shortcuts), and more.
+
